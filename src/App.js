a61549170727547900e3cde211e938a767d77c66
Uruck <102347456+Uruck@users.noreply.github.com>
2024-11-05 01:50:59 +0700

    Крестики-нолики
    Учебное задание

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import X0project from "./X0project/X0project";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Крестики-нолики</h1>
+      <X0project />
+    </div>
+  );
+}
